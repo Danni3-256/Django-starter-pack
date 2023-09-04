9c8432a12d29e806d2e33906abdfd6b7d7254149
Danni3-256 <okiadaniel90@gmail.com>
2023-09-04 22:57:43 +0300

    Initial Commit

---
+++ movies/views.py
@@ -0,0 +1,45 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from .models import Movie
+
+# data = {'movies': [
+#             {
+#                 'id': 5,
+#                 'title': 'Jaws',
+#                 'Year': 1969,
+#             },
+#             {
+#                 'id': 6,
+#                 'title': 'Sharknado',
+#                 'Year': 1980,
+#             },
+#             {
+#                 'id': 7,
+#                 'title': 'The Meg',
+#                 'Year': 2018
+#             }
+#         ]
+# }
+
+def movies(request):
+    data = Movie.objects.all()
+    # Creating dictionary to be passed to template
+    return render(request, 'movies/movies.html', {'movies': data})
+
+def home(request):
+    return HttpResponse("Home Page")
+
+def detail(request):
+    data = Movie.objects.get(pk=id)
+    return render(request, 'movies/detail.html', {'movie': data})
+
+def add(request):
+    title = request.POST.get('title')
+    year = request.POST.get('year')
+    
+    if title and year:
+        movie = Movie(title=title, year=year)
+        movie.save()
+        return HttpResponseRedirect('movies/')
+    return render(request, 'movies/add.html')
+    
